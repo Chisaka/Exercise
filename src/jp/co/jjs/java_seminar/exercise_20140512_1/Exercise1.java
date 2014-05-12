258a9d6429e76263dc1cbf42886aa66966647f3b
Chisaka <jjs-0004@outlook.jp>
2014-05-12 15:43:49 +0900

    comin

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_1/Exercise1.java
@@ -0,0 +1,24 @@
+package jp.co.jjs.java_seminar.exercise_20140512_1;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int     i  = 10;
+        double  d  = 1.1;
+        float   f  = 10.0f;
+        long    l  = 100;
+        short   s  = 5;
+        char    c  = 'a';
+        boolean b  = true;
+        byte    bt = 10;
+
+        System.out.println("int     "+ i);
+        System.out.println("double  "+ d);
+        System.out.println("float   "+ f);
+        System.out.println("long    "+ l);
+        System.out.println("short   "+ s);
+        System.out.println("char    "+ c);
+        System.out.println("boolean "+ b);
+        System.out.println("byte    "+ bt);
+    }
+
+}
