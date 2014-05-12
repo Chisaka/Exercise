ba05980d94e9e280dd4035f567faa3657906ce8a
Chisaka <jjs-0004@outlook.jp>
2014-05-12 16:12:49 +0900

    天気

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_2/Exercise2.java
@@ -0,0 +1,13 @@
+package jp.co.jjs.java_seminar.exercise_20140512_2;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        
+        String str = "曇り";
+        double d   = 24.0; 
+        
+        System.out.println("今日の札幌の天気は"+ str +"です。");
+        System.out.println("今日の札幌の気温は"+ d +"度です。");
+    }
+}
