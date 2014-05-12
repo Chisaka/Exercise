9f3b71dddc79dc15a91696c3f7b07157f9fc9b9b
Chisaka <jjs-0004@outlook.jp>
2014-05-12 16:58:46 +0900

    gerge

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_10/ec.java
@@ -0,0 +1,24 @@
+package jp.co.jjs.java_seminar.self_study.q1_10;
+
+public class ec {
+
+    public static void main(String[] args) {
+        
+        //1.10_1
+        
+        String str  = "Romeo";
+        String str2 = "where art thou?";
+        
+        System.out.println(str);
+        System.out.println(str2);
+        
+        //1.10_2
+        
+        String sei = "日本";
+        String mei = "小太郎";
+        
+        String myname = sei + mei;
+        
+        System.out.println("どうも"+ myname + "です。");
+    }
+}
