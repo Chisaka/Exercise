7ad9316223b6759211f6ac7a9213a3985418d66f
Chisaka <jjs-0004@outlook.jp>
2014-05-13 10:18:50 +0900

    exer

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_01/Exercise3.java
@@ -0,0 +1,16 @@
+package jp.co.jjs.java_seminar.exercise_20140513_01;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 3;
+        System.out.println("たし算："+ (a + b));
+        System.out.println("引き算："+ (a - b));
+        System.out.println("割り算："+ ((double)a / b));
+        System.out.println("掛け算："+ (a * b));
+        System.out.println("剰　余："+ (a % b));
+        
+    }
+
+}
