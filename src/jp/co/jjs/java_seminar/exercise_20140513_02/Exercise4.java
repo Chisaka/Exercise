493b30c30de8ca406774407dd4d260ccf995d38d
Chisaka <jjs-0004@outlook.jp>
2014-05-13 11:01:32 +0900

    exer

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_02/Exercise4.java
@@ -0,0 +1,16 @@
+package jp.co.jjs.java_seminar.exercise_20140513_02;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        
+        short a = 1000;
+        short b = 500;
+        //short c =  (short) ((short)a + b);
+        
+        int c = a + b;
+        
+        System.out.println(c);
+        
+    }
+}
