49f9ddd0a995c0090b380ae9807e7171d1ef5daf
Chisaka <jjs-0004@outlook.jp>
2014-05-13 11:15:28 +0900

    exer

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_03/Exercise5.java
@@ -0,0 +1,25 @@
+package jp.co.jjs.java_seminar.exercise_20140513_03;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+
+        //1
+        
+        byte b = 0;
+        int i = 127;
+        
+        b = (byte)i;
+        
+        System.out.println(b);
+        
+        //2
+        
+        i = 258;
+        
+        b = (byte)i;
+        
+        System.out.println(b);
+
+    }
+}
