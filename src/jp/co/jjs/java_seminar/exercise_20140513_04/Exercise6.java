33b98cc061217af74896b21505d5ce3024f078fa
Chisaka <jjs-0004@outlook.jp>
2014-05-13 11:59:18 +0900

    exer

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_04/Exercise6.java
@@ -0,0 +1,16 @@
+package jp.co.jjs.java_seminar.exercise_20140513_04;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        
+        int n = 464;
+        
+        if(1 == (n % 2)){
+            System.out.println("奇数です");
+        }
+        else
+        {
+            System.out.println("偶数です");
+        }
+    }
+}
---
+++ src/jp/co/jjs/java_seminar/self_study/q1_11/ec.java
@@ -0,0 +1,11 @@
+package jp.co.jjs.java_seminar.self_study.q1_11;
+
+public class ec {
+    public static void main(String[] args) {
+        
+        //1.11_1
+        
+        System.out.println();
+    }
+
+}
