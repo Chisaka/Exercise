53f8f1e4512f3f3fc01e28591fdb81914b623779
Chisaka <jjs-0004@outlook.jp>
2014-05-13 15:45:41 +0900

    exer

--- src/jp/co/jjs/java_seminar/exercise_20140513_08/Exercise10.java
+++ src/jp/co/jjs/java_seminar/exercise_20140513_08/Exercise10.java
@@ -3,19 +3,16 @@ package jp.co.jjs.java_seminar.exercise_20140513_08;
 public class Exercise10 {
     public static void main(String[] args) {
 
-        int fb;
         for (int i = 1; i <= 30; i++) {
-            
-            fb = i;
 
-            if (0 == fb % 3 && 0 == fb % 5) {
+            if (0 == i % 3 && 0 == i % 5) {
                 System.out.println("FizzBuzz!!");
-            } else if (0 == fb % 3) {
+            } else if (0 == i % 3) {
                 System.out.println("Fizz!");
-            } else if (0 == fb % 5) {
+            } else if (0 == i % 5) {
                 System.out.println("Buzz!");
             } else {
-                System.out.println(fb);
+                System.out.println(i);
             }
         }
     }
